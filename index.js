a7fe16f915774eb2ab8f778992180385190a61f3
cmillward13 <chris.millward13@hotmail.com>
2021-06-24 11:55:04 -0400

    fixed typo

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const core = require('@actions/core')
 const reDeploy = async ()=>{
     const url = core.getInput('url', {required:true});   
     
-    await fetch(url)
+    await fetch.fetchUrl(url)
     core.setOutput('completed', true)
 
 };
